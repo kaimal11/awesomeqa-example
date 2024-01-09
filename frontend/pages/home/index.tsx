74e140e1492c358df128d074ac323e042867bdca
kaimal11 <anuskaimal@gmail.com>
2024-01-10 00:41:42 +0100

    added functionalities to home page

--- frontend/pages/home/index.tsx
+++ frontend/pages/home/index.tsx
@@ -1,8 +1,12 @@
 import * as React from "react";
 import { NextPage } from "next";
+import Link from 'next/link'
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
-import { Button } from "@mui/material";
+import { Button, IconButton } from "@mui/material";
+import LightbulbOutlinedIcon from '@mui/icons-material/LightbulbOutlined';
+import SupportAgentOutlinedIcon from '@mui/icons-material/SupportAgentOutlined';
+import LibraryBooksOutlinedIcon from '@mui/icons-material/LibraryBooksOutlined';
 
 const Home: NextPage = () => {
 
@@ -15,15 +19,56 @@ const Home: NextPage = () => {
       <Box sx={{ flexGrow: 1, mt: 15, mb: 15 }}>
         <Grid container spacing={2}>
           <Grid item xs={12}>
-            <Box sx={{ display: "flex", justifyContent: "center" }}>
-              <Button
-                variant="contained"
-                color="primary"
-                onClick={handleClick}
-                sx={{ width: "50%", height: "4rem", fontSize: "1.2rem" }}
-              >
-                Some other Button
-              </Button>
+            <Box
+              sx={{
+                display: 'flex',
+                justifyContent: 'center',
+                gap: 2,
+                '& > div': {
+                  width: '30%',
+                  borderRadius: 2,
+                  bgcolor: '#1C1C1F',
+                  padding: 2,
+                  boxSizing: 'border-box',
+                  fontSize: '20px'
+                },
+                '& > div > a': {
+                  display: 'flex',
+                  flexDirection: 'column',
+                  alignItems: 'start',
+                }
+              }}
+            >
+              <div>
+                <Link href="/tickets">
+                  <a>
+                    <IconButton sx={{ mb: 2, bgcolor: '#302C50', borderRadius: 2 }}>
+                      <LibraryBooksOutlinedIcon />
+                    </IconButton>
+                    Knowledge Base
+                  </a>
+                </Link>
+              </div>
+              <div>
+                <Link href="/tickets">
+                  <a>
+                    <IconButton sx={{ mb: 2, bgcolor: '#302C50', borderRadius: 2 }}>
+                      <SupportAgentOutlinedIcon />
+                    </IconButton>
+                    Tickets
+                  </a>
+                </Link>
+              </div>
+              <div>
+                <Link href="/tickets">
+                  <a>
+                    <IconButton sx={{ mb: 2, bgcolor: '#302C50', borderRadius: 2 }}>
+                      <LightbulbOutlinedIcon />
+                    </IconButton>
+                    FAQ Insights
+                  </a>
+                </Link>
+              </div>
             </Box>
           </Grid>
         </Grid>
